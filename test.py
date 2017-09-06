f333a5feea15a21ca610760158f2d35460e5958c
Cyp2C19 <cyprien8@hotmail.fr>
2017-09-06 13:48:23 +0200

    test ajout fichier

--- test.py
+++ test.py
@@ -1,16 +1,21 @@
-s = "AGCTCGAGTCAGCGCTCTAGATCG"
+import random
 
-def genererReads(seq, n, k):
+seq = "AGCTCGAGTCAGCGCTCTAGATCG"
+
+def genererReads(seq, n):
+    """Permet de générer des reads en fonction d'une séquence
+       entrée en paramètre et de la taille des reads"""
     reads = []
     i = 0
     while i <= len(seq):
         if i + n > len(seq):
-            reads.append(seq[i:] + seq[:k])
             break
         else:
             reads.append(seq[i:i+n])
-            i += n-k
+            i += 1
+    #Tri aléatoire de la liste de reads
+    random.shuffle(reads)
     return reads
 
-print(s)
-print(genererReads(s, 4, 3))
+print(seq)
+print(genererReads(seq, 6))
