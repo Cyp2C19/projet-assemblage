9c1efc15c1b7a0464eda3dcade39b1fa198155e2
Cyp2C19 <cyprien8@hotmail.fr>
2017-09-06 15:30:17 +0200

    test ajout fichier

--- test.py
+++ test.py
@@ -2,24 +2,23 @@ import random
 
 seq = "ATCGCGACAAGCGCTGAGG"
 
-def genererReads(seq, n, dn, k):
-    """Permet de générer des reads de taille n en fonction d'une
-       séquence ADN entrée en paramètre """
+def genererReads(seq, n, dn, k, prof):
+    """Permet de générer des reads"""
     reads = []
-    i = 0
-    tailleReads = n
 
-    while i <= len(seq):
-        if i + tailleReads > len(seq):
-            reads.append(seq[i:])
-            break
-        else:
+    for j in range(0, prof):
+        i = 0
+        tailleReads = n
+        while i <= len(seq):
+            if i + tailleReads > len(seq):
+                reads.append(seq[i:])
+                break
             reads.append(seq[i:i+tailleReads])
             i += tailleReads - random.randint(1,k)
-    #Tri aléatoire de la liste de reads
-    random.shuffle(reads)
-    tailleReads = n + random.randint(-dn, dn)
+            tailleReads = n + random.randint(-dn, dn)
+        #Tri aléatoire de la liste de reads
+        random.shuffle(reads)
     return reads
 
 print(seq)
-print(genererReads(seq, 6, 2, 3))
+print(genererReads(seq, 6, 2, 3, 5))
