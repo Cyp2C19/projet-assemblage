5ccf522bda1d12be0c09e24adfb36f7f13442dcb
Cyp2C19 <cyprien8@hotmail.fr>
2017-09-07 10:56:27 +0200

    de Bruijn

--- suffixTree.py
+++ suffixTree.py
@@ -1,3 +1,18 @@
+# -*- coding: utf-8 -*-
+# Titre          : Partie 2 Projet Bio-Informatique. Indexation par arbres suffixes.
+# Description    : Ce programme permet de saisir une séquence d'ADN et de créer un arbre des suffixes
+#                  pour cette séquence. Plusieurs opérations sont ensuite possibles : recherche de
+#                  motifs et de suffixes, comptage du nombre d'occurences d'un motif, affichage des
+#                  suffixes par ordre alphabétique mais aussi l'affichage du motif le plus long répété
+#                  au moins deux fois.
+# Auteurs        : Babujhi, Cyprien
+# Date           : 07/04/2017
+# Version Python : 3.6
+# ──────────────────────────────────────────────────────────────────────────────────────────────────────#
+
+
+###────────────────────────────────────────── CLASSE NOEUD ──────────────────────────────────────────###
+
 class Noeud:
     """
        Classe définissant le noeud d'un arbre suffixe caractérisé par :
@@ -5,6 +20,7 @@ class Noeud:
             branche qui pointe vers le noeud enfant (=valeur)
           - Un noeud connecté au noeud courant via un lien de suffixes
     """
+
     # ───────────────────────────────────── Méthodes classe Noeud ─────────────────────────────────────#
 
     def __init__(self, lienSuffixe=None):
@@ -90,3 +106,226 @@ class ArbreSuffixe:
 
             # On avance le noeud le plus profond vers le nouveau noeud crée
             noeudPlusProfond = noeudPlusProfond.enfants[car]
+
+    def afficher(self, racine, n=0):
+        """
+           Méthode qui permet d'afficher l'arbre des suffixes
+        """
+        for i in racine.enfants.keys():
+            print(n * "  │" + "──" + i)
+            self.afficher(racine.enfants[i], n + 1)
+
+    def traverser(self, seq):
+        """
+           Méthode qui permet de traverser l'arbre en suivant les caractères d'une séquence donnée.
+           Retourne le noeud qui a une branche liant son noeud parent avec le dernier caractère
+           de la séquence donnée. Retourne None si la séquence n'est pas trouvée
+        """
+        noeudCourant = self.racine
+        for car in seq:
+            if car not in noeudCourant.enfants:
+                return None
+            noeudCourant = noeudCourant.enfants[car]
+        return noeudCourant
+
+    def chercheMotif(self):
+        """
+           Méthode qui permet de traverser l'arbre avec la méthode traverser et qui permet
+           d'afficher si la séquence donnée est un motif de la séquence cible ou non
+        """
+        print("\nRecherche de motif dans la séquence :", self.sequenceBrute())
+        seq = saisieSequence()
+        noeud = self.traverser(seq)
+        if noeud is not None:
+            print("\n", seq, "est un motif de la séquence cible")
+        else:
+            print("\n", seq, "n'est pas un motif de la séquence cible")
+
+    def chercheSuffixe(self):
+        """
+           Méthode qui permet de traverser l'arbre avec la méthode traverser et qui permet
+           d'afficher si la séquence donnée est un suffixe de la séquence cible ou non.
+           Un suffixe de l'arbre aura une branche étiquettée avec un '$' parmis ses enfants
+        """
+        print("\nRecherche de suffixe dans la séquence :", self.sequenceBrute())
+        seq = saisieSequence()
+        noeud = self.traverser(seq)
+        if noeud is not None and '$' in noeud.enfants:
+            print("\n", seq, "est un suffixe de la séquence cible")
+        else:
+            print("\n", seq, "n'est pas un suffixe de la séquence cible")
+
+    def nombreFeuilles(self, noeud):
+        """
+           Méthode qui permet de calculer le nombre de feuilles dans le sous arbre d'un
+           noeud
+        """
+        nbFeuilles = 0
+        for i in noeud.enfants.keys():
+            if i == '$':
+                nbFeuilles += 1
+            else:
+                nbFeuilles += self.nombreFeuilles(noeud.enfants[i])
+        return nbFeuilles
+
+    def nombreOccurences(self):
+        """
+           Méthode qui permet de traverser l'arbre et fficher le nombre d'occurences d'un motif donné
+           dans la chaine cible. Le nombre d'occurences correspondra au nombre de feuilles dans le sous
+           arbre du noeud trouvé si le motif est présent dans la séquence
+        """
+        print("\nRecherche du nombre d'occurences d'un motif dans la séquence :", self.sequenceBrute())
+        seq = saisieSequence()
+        noeud = self.traverser(seq)
+        if noeud != None:
+            print("\nNombre d'occurences du motif", seq, "=>", self.nombreFeuilles(noeud))
+        else:
+            print("\nLe motif n'est pas présent dans la séquence cible")
+
+    def listeSuffixes(self, racine, liste, str):
+        """
+           Méthode qui permet de parcourir tous les noeuds de l'arbre et de stocker dans
+           une liste tous les suffixes de cet arbre
+        """
+        if '$' in racine.enfants:  # Si on arrive en fin de suffixe
+            liste.append(str)
+        for i in racine.enfants.keys():
+            self.listeSuffixes(racine.enfants[i], liste, str=str + i)
+        return liste
+
+    def affichageSuffixesOrdre(self):
+        """
+           Méthode qui permet, à partir de la liste des suffixes de l'arbre, d'afficher
+           ces suffixes par ordre alphabétique
+        """
+        liste = self.listeSuffixes(self.racine, [], "")  # Liste des suffixes de l'arbre
+        liste.remove("")  # On supprime la chaine vide pour la racine
+        liste.sort()  # Tri de la liste
+
+        print("\nAffichage des suffixes de l'arbre par ordre alphabétique pour la séquence :",
+              self.sequenceBrute(), "\n")
+        for i in liste:
+            print("-", i)
+
+    def listeMotifsRepetes(self, racine, liste, str):
+        """
+           Méthode qui permet de parcourir tous les noeuds de l'arbre et de stocker dans
+           une liste tous les motifs qui sont répétés au moins deux fois dans la séquence
+           cible
+        """
+        if self.nombreFeuilles(racine) >= 2:  # Suffixe répété au moins 2 fois
+            liste.append(str)
+        for i in racine.enfants.keys():
+            self.listeMotifsRepetes(racine.enfants[i], liste, str=str + i)
+        return liste
+
+    def affichageMotifsRepetes(self):
+        """
+           Méthode qui permet, à partir de la liste des motifs répétés au moins deux fois,
+           d'afficher le/les plus long motif(s) pour cette liste
+        """
+        liste = self.listeMotifsRepetes(self.racine, [], "")  # Liste des suffixes de l'arbre
+        liste.sort(key=lambda item: len(item), reverse=True)  # Tri de la liste par tailles motifs décroissants
+        if "" in liste:
+            liste.remove("")  # Si deux feuilles à la racine on supprime le mot vide
+        if len(liste) == 0:
+            print("\nAucun motif n'est répété au moins deux fois dans la séquence : ", self.sequenceBrute())
+        else:
+            tailleMax = len(liste[0])  # Taille max du plus long motif répété deux fois
+            print("\nAffichage du/des motif(s) le(s) plus long répété(s) au moins 2 fois dans la séquence",
+                  self.sequenceBrute(), ":")
+            for i in liste:
+                if len(i) == tailleMax:  # On affiche tous les motifs de taille max
+                    print("-", i)
+
+    def sequenceBrute(self):
+        """
+           Méthode qui permet d'afficher la séquence contenue dans l'arbre sans le caractère '$'
+        """
+        seq = self.sequence
+        seq = seq.replace("$", "")
+        return seq
+
+
+###────────────────────────────────────────── SAISIES ─────────────────────────────────────────###
+
+def saisieSequence():
+    """
+        Fonction de saisie contrôlée d'une chaîne d'ADN
+        Retourne une chaine de caractères composée de A|C|T|G
+    """
+    encore = True
+    seq = ""
+    print("\nVeuillez entrer une séquence d'ADN (non sensible à la casse) => ")
+    while (encore):
+        seq = input().upper()
+        for i in seq:
+            if (i != 'A' and i != 'T' and i != 'C' and i != 'G'):
+                encore = True
+                print("Un caractère différent de A,T,C,G a été saisi, veuillez réessayer")
+                break
+            else:
+                encore = False
+    return (seq)
+
+
+###────────────────────────────────────────── MENU ─────────────────────────────────────────###
+
+def affichageMenu():
+    """
+        Fonction qui permet d'afficher le menu du programme
+    """
+    print("\n", 31 * "─", "MENU", 31 * "─")
+    print("1. Construction de l'arbre des suffixes pour une séquence d'ADN")
+    print("2. Recherche de motif")
+    print("3. Recherche de suffixe")
+    print("4. Comptage du nombre d'occurences d'un motif")
+    print("5. Motif(s) le(s) plus long répété(s) au moins deux fois")
+    print("6. Liste des suffixes triés par ordre alphabétique")
+    print("7. Quitter")
+    print(69 * "─")
+
+
+def erreurArbre():
+    """
+       Fonction qui permet d'afficher un message d'erreur dans le menu si l'arbre
+       n'a pas été crée avant d'effectuer une opération
+    """
+    print("\nVeuillez créer un arbre avant d'éffectuer cette opération\n")
+
+
+###────────────────────────────────────────── MAIN ─────────────────────────────────────────###
+
+run = True
+arbre = ArbreSuffixe("")
+
+while (run):
+    affichageMenu()
+    choix = input("Entrez votre choix [1-7] => ")
+
+    if choix == '1':
+        seq = saisieSequence()
+        arbre = ArbreSuffixe(seq)
+        arbre.construction()
+        print("\nVisualisation de l'arbre des suffixes : \n")
+        arbre.afficher(arbre.racine)
+
+    elif choix == '2':
+        arbre.chercheMotif() if arbre.sequence != "$" else erreurArbre()
+
+    elif choix == '3':
+        arbre.chercheSuffixe() if arbre.sequence != "$" else erreurArbre()
+
+    elif choix == '4':
+        arbre.nombreOccurences() if arbre.sequence != "$" else erreurArbre()
+
+    elif choix == '5':
+        arbre.affichageMotifsRepetes() if arbre.sequence != "$" else erreurArbre()
+
+    elif choix == '6':
+        arbre.affichageSuffixesOrdre() if arbre.sequence != "$" else erreurArbre()
+
+    elif choix == '7':
+        run = False
+    else:
+        print("\nLa saisie ne correspond à aucun menu, réessayez")
